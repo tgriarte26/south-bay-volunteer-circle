253615a3ddbc0b0dc9ee1902b567c575ae672541
trevor <trevorgriarte@gmail.com>
2025-06-09 01:20:50 -0700

    eleventh commit

--- src/components/home/firebase.js
+++ src/components/home/firebase.js
@@ -1,13 +1,10 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// Removed getAnalytics import and usage since it's unused
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBZ0IXotfN3n124Gk9qyh2xeLd4CvJc8B4",
   authDomain: "south-bay-volunteer-circle.firebaseapp.com",
@@ -22,6 +19,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
-const analytics = getAnalytics(app);
 
 export { auth, db };
